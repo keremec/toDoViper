e49f34fbfca1c02715479fdd1e0b74b473c76c9b
keremec <k.safa.dirican@outlook.com>
2022-08-28 17:46:57 +0300

    second commit

--- toDoViper/homepage-module/view/HomepageVC.swift
+++ toDoViper/homepage-module/view/HomepageVC.swift
@@ -11,7 +11,7 @@ class HomepageVC: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        // Do any additional setup after loading the view.s
     }
 
 
